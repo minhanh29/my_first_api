060a2f59cd139dd9fa0b7c4009e487ab57e2dc94
Minh Anh <minhanhng.sofl@gmail.com>
2021-04-08 06:16:33 +0700

    add runtime

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
 from google_trans_new import google_translator
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, request, jsonify, render_template
+# from flask_restful import Api, Resource, reqparse
 
 
 app = Flask(__name__)
-API = Api(app)
+# API = Api(app)
 
 
-class Translator(Resource):
-    @staticmethod
-    def post():
-        parser = reqparse.RequestParser()
-        parser.add_argument('text', default='')
+# class Translator(Resource):
+#     @staticmethod
+#     def post():
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('text', default='')
 
-        args = parser.parse_args()  # dict
+#         args = parser.parse_args()  # dict
 
-        sentence = args['text']
+#         sentence = args['text']
 
-        translator = google_translator()
-        trans_sentence = translator.translate(sentence, lang_tgt='vi')
+#         translator = google_translator()
+#         trans_sentence = translator.translate(sentence, lang_tgt='vi')
 
-        # print(trans_sentence)
+#         # print(trans_sentence)
 
-        output = {'translated_text': trans_sentence}
+#         output = {'translated_text': trans_sentence}
 
-        return output, 200
+#         return output, 200
 
 
-API.add_resource(Translator, '/translate')
+# API.add_resource(Translator, '/translate')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/translate', methods=['POST'])
+def post():
+    data = request.get_json(force=True)
+    sentence = data['text']
+
+    translator = google_translator()
+    trans_sentence = translator.translate(sentence, lang_tgt='vi')
+
+    # print(trans_sentence)
+
+    output = {'translated_text': trans_sentence}
+
+    return jsonify(output)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
